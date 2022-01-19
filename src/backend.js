5e77fc9163339ca06f3741b08398ffcc0bca6856
gayathri-ms <ms.gayathri266520@gmail.com>
2022-01-19 14:29:55 +0530

    updated

--- src/backend.js
+++ src/backend.js
@@ -1,2 +1,2 @@
-// export const API = "https://sss-backend1.herokuapp.com";
-export const API = "http://localhost:3000";
+export const API = "https://sss-backend1.herokuapp.com";
+// export const API = "http://localhost:3000";
